feb009229795c96ab79d163a1cd71f2254afbe2e
Athreya <akondiathreya@gmail.com>
2024-12-19 11:39:39 +0530

    images

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text style={styles.textrababu}>Rappa Rappa 😈</Text>
-            <StatusBar style="light" />
+            <Image source={{uri: "https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?t=st=1734586577~exp=1734590177~hmac=c5eae486effe7d62e6d724daf2add5da96ea3e3864ce924a77061021bdc6dfe2&w=2000"}}
+            style={{width:400, height:400}}    />
+            <Image source={require("./assets/favicon.png")} style={{width:100, height:100}}/>
+            <StatusBar style="auto" />
         </View>
     );
 }
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },

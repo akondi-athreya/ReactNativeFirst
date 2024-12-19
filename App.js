6d986d861ba61a29d8c4481c72b2faae9119ccfb
Athreya <akondiathreya@gmail.com>
2024-12-19 11:13:35 +0530

    opened in code editor

--- App.js
+++ App.js
@@ -2,19 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textrababu}>Rappa Rappa 😈</Text>
+            <StatusBar style="light" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textrababu: {
+        color: '#fff',
+        fontSize: 32,
+    }
 });

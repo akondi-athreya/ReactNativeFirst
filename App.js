bd70ac8765da04c459eb5a65c7ce2b61775eacb6
Athreya <119722868+akondi-athreya@users.noreply.github.com>
2024-12-23 11:06:10 +0530

    Done FlatList

--- App.js
+++ App.js
@@ -1,43 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
 import React from 'react';
 
 export default function App() {
-    const [name, setName] = React.useState('');
-    const [pass, setPass] = React.useState('');
-    function kinguu(val) {
-        setName(val);
-    }
-    function queenuu(val) {
-        setPass(val);
-    }
-    const showModel = () => {
-        Alert.alert('Login Details', `Username: ${name}\nPassword: ${pass}`);
-    }
-
+    const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return (
         <View style={styles.container}>
-            {/* <Image source={{uri: "https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?t=st=1734586577~exp=1734590177~hmac=c5eae486effe7d62e6d724daf2add5da96ea3e3864ce924a77061021bdc6dfe2&w=2000"}}
-            style={{width:400, height:400}}    />
-            <Image source={require("./assets/favicon.png")} style={{width:100, height:100}}/> */}
-            <View style={styles.rr}>
-                <View style={styles.inputContainer}>
-                    <Text style={styles.label}>Username</Text>
-                    <TextInput
-                        style={styles.input} onChangeText={(text) => kinguu(text)} value={name}
-                        placeholder="Enter your Username" placeholderTextColor="#999"
-                    />
-                    <Text style={styles.label}>Password</Text>
-                    <TextInput
-                        style={styles.input} onChangeText={(text) => queenuu(text)} value={pass} secureTextEntry
-                        placeholder="Enter your Password" placeholderTextColor="#999"
-                    />
-                </View>
-                <View>
-                    <Button title='login' onPress={() => showModel()} />
-                </View>
+            <View>
+                <Text style={styles.fontt}>Hello !</Text>
             </View>
-            <StatusBar style="inverted" />
+            <View>
+                <FlatList
+                    data={data}
+                    renderItem={({ item }) => <View style={styles.box}>
+                        <Text style={styles.fontt}>{item}</Text>
+                    </View>}
+                    numColumns={4}
+                />
+            </View>
+            <StatusBar style="auto" />
         </View>
     );
 }
@@ -46,35 +27,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#264653',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    textrababu: {
-        color: 'white',
-        fontSize: 32,
+    fontt: {
+        color: '#fff',
     },
-    rr: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '80%',
-        marginBottom: 10,
-    },
-    label: {
-        fontSize: 16,
-        color: '#bde0fe',
-        marginBottom: 5,
-    },
-    input: {
-        height: 40,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 10,
-        paddingLeft: 10,
-        backgroundColor: '#1b263b',
-        marginBottom: 20,
-        color: '#fff'
-    },
+    box: {
+        height: 'fit-content',
+        width: '20%',
+        padding: 10,
+        borderWidth: 2,
+        borderColor: 'red',
+        margin: 10,
+    }
 });
